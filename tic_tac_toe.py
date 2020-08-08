a0bd5416ae0adaf3063a0398ff904eadeec870d9
Lucas Clans <lucasclans@zoho.com>
2020-08-08 12:08:07 -0300

    more readable

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,13 +2,7 @@
 
 from os import system
 
-game_board = ['.','.','.',
-              '.','.','.',
-              '.','.','.']
-player = 1
-round_counter = 0
-
-def display_game():
+def display_game(game_board):
     print(f' {game_board[6]} | {game_board[7]} | {game_board[8]}')
     print('--- --- ---')
     print(f' {game_board[3]} | {game_board[4]} | {game_board[5]}')
@@ -25,8 +19,7 @@ def display_exemple():
     print(f' 1 | 2 | 3')
 
 
-def change_player():
-    global player
+def change_player(player):
 
     if player == 1:
         print("Now it's O turn!")
@@ -34,12 +27,9 @@ def change_player():
     if player == -1:
         print("Now it's X turn")
 
-    player = player * -1
+    return player * -1
 
-
-def do_a_play():
-    global player
-    global round_counter
+def do_a_play(player, round_counter):
 
     display_exemple()
     get_play = input('Choose a position between 1 and 9 to play')
@@ -52,43 +42,54 @@ def do_a_play():
         if player == -1:
             game_board[play - 1] = 'O'
 
-        round_counter = round_counter + 1
+        round_counter += 1
 
     else:
         system('clear')
         print('You can not play there')
-        do_a_play()
+        do_a_play(player.round_counter)
 
     system('clear')
 
 
-def check_win():
+def check_win(game_board):
 
-    # Check horizontal win
-    if game_board[0] == game_board[1] == game_board[2] != '.' or game_board[3] == game_board[4] == game_board[5] != '.' or game_board[6] == game_board[7] == game_board[8] != '.':
-        print(f'We have a winner!')
-        exit()
+    horizontal_win = game_board[0] == game_board[1] == game_board[2] != '.' or game_board[3] == game_board[4] == game_board[5] != '.' or game_board[6] == game_board[7] == game_board[8] != '.'
+    vertical_win   = game_board[0] == game_board[3] == game_board[6] != '.' or game_board[1] == game_board[4] == game_board[7] != '.' or game_board[2] == game_board[5] == game_board[8] != '.'
+    diagonal_win   = game_board[0] == game_board[4] == game_board[8] != '.' or game_board[2] == game_board[4] == game_board[6] != '.'
 
-    # Check vertical win
-    if game_board[0] == game_board[3] == game_board[6] != '.' or game_board[1] == game_board[4] == game_board[7] != '.' or game_board[2] == game_board[5] == game_board[8] != '.':
-        print(f'We have a winner!')
-        exit()
+    win  = horizontal_win or vertical_win or diagonal_win
+    draw = round_counter == 9
 
-    # Check diagonal win
-    if game_board[0] == game_board[4] == game_board[8] != '.' or game_board[2] == game_board[4] == game_board[6] != '.':
-        print(f'We have a winner!')
-        exit()
+    if win:
+        return 'winner'
 
-    # Check draw
-    elif round_counter == 9:
-        print(f'It is a draw!')
-        exit()
+    elif draw:
+        return 'draw'
 
-    change_player()
+    else:
+        return False
 
+player = 1
+round_counter = 0
+
+game_board = ['.','.','.',
+              '.','.','.',
+              '.','.','.']
 
 while True:
-    check_win()
-    do_a_play()
-    display_game()
+
+    player = change_player(player)
+
+    print(player)
+
+    result = check_win(game_board)
+
+    if result:
+        print(f'Game over! We have a {result}')
+        break
+
+    do_a_play(player, round_counter)
+
+    display_game(game_board)
 
